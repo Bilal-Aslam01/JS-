0b5aa47fb95aec06b247b5aa28cf456a52e83a87
Bilal Aslam <154507842+Bilal-Aslam01@users.noreply.github.com>
2024-03-02 15:28:51 +0500

    JS Chapter 1, 2, 3

---
+++ app.js
@@ -0,0 +1,150 @@
+
+
+
+Swal.fire("Yah! I Can Write HTML Content Through JavaScript!");
+
+// document.write("Yah! I Can Write HTML Content Through JavaScript.", "<br/>", "<br/>")
+
+
+let myVariable = "HELLO WORLD";
+// let myNum = "123";
+let result = myVariable;
+
+console.log(result)
+
+document.write(result, "<br/>", "<br/>")
+
+alert(result)
+
+
+
+
+
+let userNAme = "Bilal";
+
+let $name = userNAme;
+
+console.log($name)
+
+document.write($name, "<br/>", "<br/>")
+
+alert($name)
+
+
+
+
+let fullName = "Bilal Aslam";
+
+let name  = fullName;
+
+console.log(name)
+
+document.write(name,"<br/>", "<br/>")
+
+alert(name)
+
+
+
+
+let age = "23";
+
+let myAge  = age;
+
+console.log(myAge)
+
+document.write("I Am ", myAge, " Years Old",  "<br/>", "<br/>")
+
+
+alert("I Am 23 Years Old")
+
+
+
+
+let dob = "September 19,2001";
+
+let MyDob  = dob;
+
+console.log(MyDob)
+
+document.write("My Birthdate Is ",MyDob)
+
+
+alert("My Birthdate Is September 19,2001")
+
+
+
+
+
+let course = "Certified Mobile Application Development";
+
+let MyCourse  = course;
+
+console.log(MyCourse)
+
+document.write(MyCourse, "<br/>", "<br/>")
+
+alert(MyCourse)
+
+
+
+
+
+document.write("PIZZA\n<br/>PIZZ\n<br/>PIZ\n<br/>PI\n<br/>P", "<br/>", "<br/>")
+
+
+
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+
+
+
+
+let email = "Aslamb481@gmail.com";
+
+let MyEmail  = email;
+
+console.log(MyEmail)
+
+document.write("My Email is ", MyEmail, "<br/>", "<br/>")
+
+
+
+
+alert("My Email is Aslamb481@gmail.com")
+
+
+
+
+
+let book = "A smarter way to learn JavaScript.";
+
+let MyBook  = book;
+
+console.log(MyBook)
+
+document.write(MyBook, "<br/>", "<br/>")
+
+alert( MyBook)
+
+
+
+
+
+
+
+
+
+
+let style = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+let MyStyle  = style;
+
+document.write(MyStyle, "<br/>", "<br/>")
+
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+
+
+
+
